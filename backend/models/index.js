e10eb59e0aae337783c2bc1d743361a82baad848
NthreeAhn <a.hyunjun1999@gmail.com>
2022-10-30 22:22:57 +0900

    (chore)backend: backend/models/index.js db 연동 환경변수 부분 수정

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const Sequelize = require('sequelize');
 
-const env = 'development';
+const env = process.env.NODE_ENV || 'development';
 const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 const db = {};
 
