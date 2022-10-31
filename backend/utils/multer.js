53f8ed08094ecb2f757e7331670f8b45cdd70457
NthreeAhn <a.hyunjun1999@gmail.com>
2022-10-31 18:16:10 +0900

    chore(backend) multer 라이브러리 관련 설정, routes 폴더 아래 api 파일 생성 및 설정

---
+++ backend/utils/multer.js
@@ -0,0 +1,59 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require('fs');
+
+try{
+    fs.readdirSync( path.join(__dirname, "../../img/profiles"));
+}catch(err){
+    console.error("img/profiles가 없어서 폴더를 생성합니다");
+    fs.mkdirSync(path.join(__dirname, "../../img/profiles"));
+}
+
+try{
+    fs.readdirSync( path.join(__dirname, "../../img/projects"));
+}catch(err){
+    console.error("img/projects가 없어서 폴더를 생성합니다");
+    fs.mkdirSync(path.join(__dirname, "../../img/projects"));
+}
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg"){
+        cb(null, true);
+    }
+    else{
+        req.fileValidationError = "이미지 파일 확장자 확인 부탁드립니다(허용 확장자: jpg, jpeg, png, gif)";
+        cb(null, false);
+    }
+};
+
+const uploadProfile = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, done) => {
+            done(null, path.join(__dirname, "../../img/profiles"));
+        },
+        filename: (req, file, done) => {
+            const ext = path.extname(file.originalname);
+            const fileName = path.basename(file.originalname, ext) + Date.now() + ext;
+            done(null, fileName);
+        },
+    }),
+    fileFilter: fileFilter,
+    limits: {fileSize: 30*1024*1024},
+});
+
+const uploadProject = multer({
+    storage: multer.diskStorage({
+        destination: (req, file, done) => {
+            done(null, path.join(__dirname, "../../img/projects"));
+        },
+        filename: (req, file, done) => {
+            const ext = path.extname(file.originalname);
+            const fileName = path.basename(file.originalname, ext) + Date.now() + ext;
+            done(null, fileName);
+        },
+    }),
+    fileFilter: fileFilter,
+    limits: {fileSize: 30*1024*1024},
+});
+
+module.exports = {uploadProfile, uploadProject};
