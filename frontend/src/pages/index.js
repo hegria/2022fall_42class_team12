bf3fefa23b703ed0c8058845255acefcca209e58
Kangdyu <kangdyu99@gmail.com>
2022-11-04 18:39:09 +0900

    feat(frontend): 최근 등록된 모집글 섹션 레이아웃

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,7 @@
 import HeroSection from "components/pages/home/sections/HeroSection";
 import PopularProjectsSection from "components/pages/home/sections/PopularProjectsSection";
 import RandomProjectsSection from "components/pages/home/sections/RandomProjectsSection";
+import RecentProjectsSection from "components/pages/home/sections/RecentProjectsSection";
 
 function HomePage() {
   return (
@@ -8,6 +9,7 @@ function HomePage() {
       <HeroSection />
       <RandomProjectsSection />
       <PopularProjectsSection />
+      <RecentProjectsSection />
     </>
   );
 }
