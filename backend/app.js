7e5ac3282a129efce64ce42d20a3f194d0d76eff
NthreeAhn <a.hyunjun1999@gmail.com>
2022-11-06 12:59:41 +0900

    chore(backend) /backend/app.js 부분에 DB 관련 주의점 주석으로 남김

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,11 @@ const app = express();
 require('dotenv').config();
 
 const sequelize = require('./models').sequelize;
+
+// 여기에서 force: true면 db 날라가서 나중에 이거 어느정도 
+// 테이블 스키마가 다 완료되면
+// force: false로 바꿔줘야 함 안그러면 우리가 차곡차곡 모은 데이터가
+// 다 날라감!!!!
 sequelize.sync({force: true}).then(()=> {
     console.log('database connect');
 }).catch((err)=> {
