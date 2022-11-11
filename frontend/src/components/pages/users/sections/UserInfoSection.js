0a21f68b4f81962f25a0a9848db25f191d8636bf
Kangdyu <kangdyu99@gmail.com>
2022-11-11 20:25:02 +0900

    feat(frontend): 유저 기본 정보 섹션 레이아웃

--- frontend/src/components/pages/users/sections/UserInfoSection.js
+++ frontend/src/components/pages/users/sections/UserInfoSection.js
@@ -1,10 +1,49 @@
-import { Box, Container } from "@chakra-ui/react";
+import { EmailIcon, InfoIcon } from "@chakra-ui/icons";
+import { Avatar, Box, Center, Container, HStack, Tag, Text, VStack, Wrap } from "@chakra-ui/react";
+import { MOCKUP_USER } from "constants/mockups/user";
+import { useState } from "react";
 
 function UserInfoSection() {
+  const [data, setData] = useState(MOCKUP_USER);
+
   return (
     <Box as="section" marginTop="80px">
-      <Container maxW="container.xl" paddingY="80px">
-        User info section
+      <Container maxW="container.md" paddingY="80px">
+        <Center flexDirection="column" rowGap="24px">
+          <Avatar size="2xl" src={data.photoUrl} />
+
+          <VStack spacing="8px">
+            <Text fontSize="24px" fontWeight="bold">
+              {data.name}
+            </Text>
+            <Text fontSize="14px" fontWeight="bold" color="gray.700">
+              {data.department}
+            </Text>
+            <Text fontSize="16px" color="gray.700">
+              {data.introduction}
+            </Text>
+          </VStack>
+
+          <Wrap justify="center">
+            {data.skills.map((skill, idx) => (
+              <Tag key={idx}>{skill}</Tag>
+            ))}
+          </Wrap>
+
+          <VStack spacing="4px">
+            <HStack spacing="8px">
+              <EmailIcon w="16px" h="16px" />
+              <Text fontSize="14px">{data.email}</Text>
+            </HStack>
+
+            {data.personalLink && (
+              <HStack spacing="8px">
+                <InfoIcon w="16px" h="16px" />
+                <Text fontSize="14px">{data.personalLink}</Text>
+              </HStack>
+            )}
+          </VStack>
+        </Center>
       </Container>
     </Box>
   );
