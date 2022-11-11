0c660a8abadf365ca8b399d02b997885869d8a89
Kangdyu <kangdyu99@gmail.com>
2022-11-11 20:56:53 +0900

    feat(frontend): UserCard 레이아웃 수정, 프로필 보기 버튼 눌렀을 때만 이동하도록 변경

--- frontend/src/components/common/UserCard.js
+++ frontend/src/components/common/UserCard.js
@@ -1,67 +1,46 @@
-import {
-  Avatar,
-  Box,
-  chakra,
-  Button,
-  Flex,
-  HStack,
-  Icon,
-  Tag,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
-
-const Anchor = chakra("a", {
-  baseStyle: {
-    cursor: "pointer",
-    _hover: {
-      transform: "translate(-2px, -2px)",
-    },
-    transition: "transform 0.2s",
-  },
-});
+import { Avatar, Box, Button, Flex, HStack, Tag, Text, VStack } from "@chakra-ui/react";
+import Link from "next/link";
 
 function UserCard({ profimg, name, department, skills, intro }) {
   return (
-    <Anchor>
-      <Box
-        minW="360px"
-        w="100%"
-        borderRadius="24px"
-        bgColor="white"
-        boxShadow="base"
-        overflow="hidden"
-      >
-        <Flex direction="column">
-          <Flex direction="column" padding="16px" rowGap="24px">
-            <VStack spacing="8px" align="flex-start">
-              <HStack spacing="18px">
-                <Avatar bg="gray.300" size="lg" name={name} src={profimg} />
-                <VStack spacing="2px">
-                  <Text fontSize="2xl" fontWeight="bold">
-                    {name}
-                  </Text>
-                  <Text fontSize="sm" color="gray.600">
-                    {department}
-                  </Text>
-                </VStack>
-              </HStack>
-            </VStack>
-            <HStack spacing="6px">
-              {skills.map((skill, idx) => (
-                <Tag key={idx}>{skill}</Tag>
-              ))}
+    <Box
+      minW="360px"
+      w="100%"
+      borderRadius="24px"
+      bgColor="white"
+      boxShadow="base"
+      overflow="hidden"
+    >
+      <Flex direction="column">
+        <Flex direction="column" padding="16px" rowGap="24px">
+          <VStack spacing="8px" align="flex-start">
+            <HStack spacing="18px">
+              <Avatar bg="gray.300" size="lg" name={name} src={profimg} />
+              <VStack spacing="2px" align="flex-start">
+                <Text fontSize="2xl" fontWeight="bold">
+                  {name}
+                </Text>
+                <Text fontSize="sm" color="gray.600">
+                  {department}
+                </Text>
+              </VStack>
             </HStack>
-            <Text fontSize="sm">{intro}</Text>
+          </VStack>
+          <HStack spacing="6px">
+            {skills.map((skill, idx) => (
+              <Tag key={idx}>{skill}</Tag>
+            ))}
+          </HStack>
+          <Text fontSize="sm">{intro}</Text>
 
-            <Button colorScheme="gray" size="lg">
-              {" "}
-              프로필 보기{" "}
+          <Link href={`/users/${1}`} passHref>
+            <Button as="a" colorScheme="gray" size="lg">
+              프로필 보기
             </Button>
-          </Flex>
+          </Link>
         </Flex>
-      </Box>
-    </Anchor>
+      </Flex>
+    </Box>
   );
 }
 
