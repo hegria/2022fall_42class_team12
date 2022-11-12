ae6a589deb1d8ca14cd26d1ef90c438869b897da
Kangdyu <kangdyu99@gmail.com>
2022-11-12 23:15:06 +0900

    feat(frontend): 모집글 전체보기 페이지 api 연동

--- frontend/src/pages/recruitments/index.js
+++ frontend/src/pages/recruitments/index.js
@@ -10,16 +10,19 @@ import {
   SimpleGrid,
 } from "@chakra-ui/react";
 import HomeSectionLayout from "components/pages/home/HomeSectionLayout";
-import { MOCKUP_PROJECT_LIST } from "constants/mockups/project";
 import ProjectCard from "components/common/ProjectCard";
 import { PAGE_SIZE } from "constants/list";
 import useRecruitments from "components/hooks/useRecruitments";
+import { useState } from "react";
 
 function RecruitPage() {
+  const [keywordInput, setKeywordInput] = useState("");
+
+  // TODO: Debouncing
   const { recruitments, size, setSize, initialLoading, loadingMore, empty, reachingEnd } =
     useRecruitments({
       pageSize: PAGE_SIZE,
-      sortBy: "favorite.desc",
+      keyword: keywordInput,
     });
 
   return (
@@ -37,29 +40,42 @@ function RecruitPage() {
               // eslint-disable-next-line react/no-children-prop
               children={<SearchIcon color="gray.500" />}
             />
-            <Input variant="filled" placeholder="검색어를 입력하세요" w="800px" />
+            <Input
+              variant="filled"
+              placeholder="검색어를 입력하세요"
+              w="800px"
+              value={keywordInput}
+              onChange={(e) => setKeywordInput(e.target.value)}
+            />
           </InputGroup>
         </Center>
       </Center>
       <VStack spacing="40px">
-        <SimpleGrid columns={3} spacing="24px" w="100%">
-          {MOCKUP_PROJECT_LIST.content.map((project) => (
-            <ProjectCard
-              key={project.id}
-              projectId={project.id}
-              subject={project.subject}
-              title={project.title}
-              startDate={project.startDate}
-              skills={project.skills}
-              photoUrl={project.photoUrl}
-              authorPhotoUrl={project.author.photoUrl}
-              authorName={project.author.name}
-              maxPeopleCount={project.capacity}
-              curPeopleCount={project.approvalCount}
-            />
-          ))}
-        </SimpleGrid>
-        <Button size="lg">더 불러오기</Button>
+        {!initialLoading && empty ? (
+          <Center>등록된 모집글이 없습니다.</Center>
+        ) : (
+          <SimpleGrid columns={3} spacing="24px" w="100%">
+            {recruitments.map((project) => (
+              <ProjectCard
+                key={project.id}
+                projectId={project.id}
+                subject={project.subject}
+                title={project.title}
+                startDate={project.startDate}
+                skills={project.skills}
+                photoUrl={project.photoUrl}
+                authorPhotoUrl={project.author.photoUrl}
+                authorName={project.author.name}
+                maxPeopleCount={project.capacity}
+                curPeopleCount={project.approvalCount}
+              />
+            ))}
+          </SimpleGrid>
+        )}
+
+        <Button onClick={() => setSize(size + 1)} size="lg" disabled={loadingMore || reachingEnd}>
+          {loadingMore ? "불러오는 중..." : reachingEnd ? "데이터 없음" : "더 불러오기"}
+        </Button>
       </VStack>
     </HomeSectionLayout>
   );
