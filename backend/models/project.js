4b4e2eef8049795d0f90ebf654501ce751904ef1
Kangdyu <kangdyu99@gmail.com>
2022-11-13 12:52:28 +0900

    fix(backend): image field 길이가 짧아서 터지던 현상 수정

--- backend/models/project.js
+++ backend/models/project.js
@@ -57,7 +57,7 @@ module.exports = function(sequelize) {
         },
         image:{
             field: 'image',
-            type: DataTypes.STRING,
+            type: DataTypes.STRING(10000),
             allowNull: true,
         },
         message:{
